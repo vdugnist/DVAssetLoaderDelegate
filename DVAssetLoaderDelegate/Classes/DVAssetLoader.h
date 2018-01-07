a03f96d144816c1a0f38d8416b3867d4d724eb2a
vdugnist <vdugnist@gmail.com>
2018-01-07 13:04:07 +0300

    add header file

---
+++ DVAssetLoaderDelegate/Classes/DVAssetLoader.h
@@ -0,0 +1,16 @@
+//
+//  DVAssetLoader.h
+//  DVAssetLoaderDelegate
+//
+//  Created by Vladislav Dugnist on 07/01/2018.
+//
+
+#ifndef DVAssetLoader_h
+#define DVAssetLoader_h
+
+#import "DVAssetLoaderDelegatesDelegate.h"
+#import "DVURLAsset.h"
+#import "DVAssetLoaderDelegate.h"
+#import "DVAssetLoaderHelpers.h"
+
+#endif /* DVAssetLoader_h */
