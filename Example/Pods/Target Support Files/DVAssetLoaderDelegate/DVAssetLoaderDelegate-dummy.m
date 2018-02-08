4aba9ce2b99555d297f8eb938c2075fe5cf2ea10
vdugnist <vdugnist@gmail.com>
2018-02-08 14:45:49 +0300

    remove pod folder from git

--- Example/Pods/Target Support Files/DVAssetLoaderDelegate/DVAssetLoaderDelegate-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_DVAssetLoaderDelegate : NSObject
-@end
-@implementation PodsDummy_DVAssetLoaderDelegate
-@end
--- Example/Pods/Target Support Files/Pods-DVAssetLoaderDelegate_Example/Pods-DVAssetLoaderDelegate_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DVAssetLoaderDelegate_Example : NSObject
-@end
-@implementation PodsDummy_Pods_DVAssetLoaderDelegate_Example
-@end
